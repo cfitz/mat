11384967fc03da483dbe1b7360763dad4b50f134
Chris Fitzpatrick <cfitz@stanford.edu>
2011-07-12 12:41:20 -0700

    beefing specs upto 100%

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,9 +6,12 @@ require 'rack/test'
 require 'rspec'
 require 'equivalent-xml'
 require 'equivalent-xml/rspec_matchers'
+require 'fakeweb'
+
 
 # set test environment
 set :environment, :test
 set :run, false
 set :raise_errors, true
 set :logging, false
+
